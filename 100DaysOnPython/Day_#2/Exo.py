87a19a1ff355c48cbd169efd69a1dc5d57b768a3
Mistane98 <Mamadoughouranebah@gmail.com>
2025-03-15 21:53:45 +0100

    j'ai ajouté le 3ème exo

--- 100DaysOnPython/Day_#2/Exo.py
+++ 100DaysOnPython/Day_#2/Exo.py
@@ -1,7 +1,19 @@
 # Déclare une variable nom contenant ton prénom et affiche-la avec print().
+
 # nom = "Ton prénom"
 # print(nom)
 # print(nom)
 
-age = 67
-print("j'ai",age,"ans")
+
+# Crée une variable age, attribue-lui un nombre et affiche "J'ai X ans" en 
+# remplaçant X par la valeur de age.
+
+# age = 67
+# print("j'ai",age,"ans")
+
+
+# Déclare une variable ville contenant le nom de ta ville et affiche "J'habite 
+# à ...".
+
+ville = "Conakry"
+print("J'habite",ville)
