070f2fecfed9f2c73f96b9dc716b7ee54e85131c
Mistane98 <Mamadoughouranebah@gmail.com>
2025-03-15 21:42:45 +0100

    j'ai modifié le 1er exo et crée un autre dossier ou il y'a index.html

--- 100DaysOnPython/Day_#2/Exo.py
+++ 100DaysOnPython/Day_#2/Exo.py
@@ -1,4 +1,7 @@
 # Déclare une variable nom contenant ton prénom et affiche-la avec print().
-nom = "Ton prénom"
-print(nom)
-print(nom)
+# nom = "Ton prénom"
+# print(nom)
+# print(nom)
+
+age = 67
+print("j'ai",age,"ans")
